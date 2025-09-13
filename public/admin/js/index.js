9a2ddf24cc1b49c8fce7c623b81027c438fc6f60
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-09-13 21:33:48 +0600

    - temp login test for admin

--- public/admin/js/index.js
+++ public/admin/js/index.js
@@ -7,7 +7,8 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     if (!response.ok) {
       if (window.location.pathname !== '/login.html') {
-        window.location.replace('/login.html');
+        // window.location.replace('/login.html');
+        console.log(`unable to chheck login`)
       }
       return;
     }
