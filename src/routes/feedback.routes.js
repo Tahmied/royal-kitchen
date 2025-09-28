822e02e562c429e1c1e7fdbde245aa1714240083
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-09-28 12:36:07 +0600

    - fixed some header issues

--- src/routes/feedback.routes.js
+++ src/routes/feedback.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { createFeedback } from "../controllers/feedback.controller.js";
+import { adminAuth } from "../middlewares/auth.middleware.js";
 import { mediaUpload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
-router.post('/create-feedback', mediaUpload('feedback').fields([
+router.post('/create-feedback', adminAuth, mediaUpload('feedback').fields([
         { name: 'feedbackClientImage', maxCount: 1 },
         { name: 'feedbackLogoImage', maxCount: 1 },
         { name: 'feedbackRightImage', maxCount: 1 },
